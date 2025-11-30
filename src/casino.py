d65b42e8919057aa3571e8e36fded673497a4d36
futo <fedor.alekseev.msk@gmail.com>
2025-11-30 11:57:03 +0300

    Add player & goose collections without indexation and iteration

--- src/casino.py
+++ src/casino.py
@@ -1,25 +1,21 @@
-from src.users import Player, Goose, WarGoose, HonkGoose
+from src.users import Player, PlayerCollection, Goose, GooseCollection, WarGoose, HonkGoose
 
 
 class Casino:
     def __init__(self) -> None:
-        self.player_list: dict[str, Player] = {}
-        self.goose_list: dict[str, Goose | WarGoose | HonkGoose] = {}
+        self.players: PlayerCollection = PlayerCollection()
+        self.gooses: GooseCollection = GooseCollection()
 
     def add_user(self, user: Player | Goose | WarGoose | HonkGoose) -> None:
         if type(user) is Player:
-            self.player_list[user.name] = user
-        elif type(user) is Goose | WarGoose | HonkGoose:
-            self.goose_list[user.name] = user
+            self.players.__add__(user)
+        elif isinstance(user, (Goose | WarGoose | HonkGoose)):
+            self.gooses.__add__(user)
         else:
             raise TypeError(f"{user} isn't a player nor goose")
 
     def show_players(self) -> str:
-        if not self.player_list:
-            return "Currently no players"
-        return "\n".join([b.about() for a, b in self.player_list.items()])
+        return self.players.show()
 
     def show_gooses(self) -> str:
-        if not self.goose_list:
-            return "Currently no gooses"
-        return "Gooses and their honk volumes\n" + "\n".join([b.about() for a, b in self.goose_list.items()])
+        return self.gooses.show()
--- src/users.py
+++ src/users.py
@@ -1,3 +1,4 @@
+from random import randint
 from src.money import CasinoBalance
 
 
@@ -18,6 +19,14 @@ class Goose:
     def about(self) -> str:
         return f"{self.name}: {self.honk_volume}"
 
+    def steal_attempt(self, player: Player):
+        dice = randint(0, 5) * 10
+        amount = player.balance.current_value()
+        if amount > dice:
+            amount = dice
+        player.balance.__setitem__(player.name, -amount)
+        return f"{self.name} has stolen {amount} from {player.name}"
+
 
 class WarGoose(Goose):
     def attack(self, player: Player) -> None:
@@ -29,3 +38,57 @@ class HonkGoose(Goose):
     def scream(self, player: Player) -> None:
         player.balance.__setitem__(player.name, int(-100 * self.honk_volume))
         return
+
+
+class PlayerCollection:
+    def __init__(self) -> None:
+        self.dict: dict[str, Player] = {}
+
+    def __add__(self, player: Player) -> None | str:
+        if type(player) is not Player:
+            raise IndexError(f"{player} is not a player!")
+        elif player.name not in self.dict:
+            self.dict[player.name] = player
+            return None
+        else:
+            return f"{player.name} already added"
+
+    def remove(self, player: Player) -> None | str:
+        if type(player) is not Player:
+            raise IndexError(f"{player} is not a player!")
+        elif not self.dict[player.name]:
+            return f"{player.name} is not in collection"
+        else:
+            self.dict.pop(player.name)
+            return None
+
+    def show(self) -> str:
+        if not self.dict:
+            return "Currently no players"
+        return "\n".join([b.about() for a, b in self.dict.items()])
+
+
+class GooseCollection:
+    def __init__(self) -> None:
+        self.dict: dict[str, Goose | WarGoose | HonkGoose] = {}
+
+    def __add__(self, goose: Goose | WarGoose | HonkGoose):
+
+        if goose.name not in self.dict:
+            self.dict[goose.name] = goose
+            return None
+        else:
+            return f"{goose.name} already added"
+
+    def remove(self, goose: Goose | WarGoose | HonkGoose) -> None | str:
+
+        if not self.dict[goose.name]:
+            return f"{goose.name} is not in collection"
+        else:
+            self.dict.pop(goose.name)
+            return None
+
+    def show(self) -> str:
+        if not self.dict:
+            return "Currently no gooses"
+        return "\n".join([b.about() for a, b in self.dict.items()])
